fc750da0731ae6d35ec50a2b65a8a4ea8d5a83e7
brunopeople <brunopessoa.melo@gmail.com>
2017-11-13 16:18:11 -0200

    carregando o autoload e a hcode class erro no composer update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,40 +10,10 @@ class ComposerStaticInitfbcfbc4f5cab6809a2f78e07f410d3d6
         0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
     );
 
-    public static $prefixesPsr0 = array (
-        'S' => 
-        array (
-            'Slim' => 
-            array (
-                0 => __DIR__ . '/..' . '/slim/slim',
-            ),
-        ),
-        'R' => 
-        array (
-            'Rain' => 
-            array (
-                0 => __DIR__ . '/..' . '/rain/raintpl/library',
-            ),
-        ),
-    );
-
-    public static $classMap = array (
-        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
-        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
-        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
-        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
-        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
-        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
-        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->fallbackDirsPsr4 = ComposerStaticInitfbcfbc4f5cab6809a2f78e07f410d3d6::$fallbackDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitfbcfbc4f5cab6809a2f78e07f410d3d6::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInitfbcfbc4f5cab6809a2f78e07f410d3d6::$classMap;
 
         }, null, ClassLoader::class);
     }
